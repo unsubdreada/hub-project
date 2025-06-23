33395d3a443acdb4fdac4b31bca48c682f4ec78c
unsubdreada <shura74084@gmail.com>
2025-06-23 10:44:37 +0500

    Удалил ESlint

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/styles/_importer.scss" as *;`
-      }
-    }
+        additionalData: `@use "@/styles/_importer.scss" as *;`,
+      },
+    },
   },
   resolve: {
     alias: {
       '@': path.resolve('./src'),
       '@components': path.resolve('./src/components'),
       '@views': path.resolve('./src/views'),
-      '@styles': path.resolve('./src/styles')
-    }
+      '@styles': path.resolve('./src/styles'),
+    },
   },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
-  }
-})
+    setupFiles: './src/setupTests.js',
+  },
+});
